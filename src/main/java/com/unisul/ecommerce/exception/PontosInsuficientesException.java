4c81af9f1a3b1d514374c391545ba98801dfe3fe
Matheus Picolli Ishibashi <matheus.ishibashi@gmail.com>
2026-04-24 12:38:26 -0300

    feat: implementa modelos defensivos e excecoes de negocio

--- src/main/java/com/unisul/ecommerce/exception/PontosInsuficientesException.java
+++ src/main/java/com/unisul/ecommerce/exception/PontosInsuficientesException.java
@@ -1,8 +1,10 @@
 package com.unisul.ecommerce.exception;
 
 public class PontosInsuficientesException extends RuntimeException {
-    public PontosInsuficientesException(String message) {
-        super(message);
+    public PontosInsuficientesException(int pontosNecessarios, int pontosAtuais) {
+        super("Saldo insuficiente! Você tentou usar " + pontosNecessarios +
+                " pontos, mas só possui " + pontosAtuais
+                + ".");
     }
 
 }
